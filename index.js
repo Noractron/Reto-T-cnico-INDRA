0b52e316948dfec8fe0cd05f1d7d213a6fca2e28
jonathan <noractron@gmail.com>
2021-01-07 11:51:38 -0500

    reto tecnico

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const awsServerlessExpress = require('aws-serverless-express');
-const app = require('./app');
+var awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const express = require('express');
+var bodyParser = require('body-parser')
+const app = express();
+var swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger.json');
+
+// declaramos una nueva express app
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(awsServerlessExpressMiddleware.eventContext())
+
+// habilitar todos los metodos para el CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+  next()
+});
+
+//Endpoint para el swagger
+app.use('/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+require('./routes')(app);
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+}));
+
+// const port = parseInt(process.env.PORT, 10) || 3000;
+// server.listen(port);
 const server = awsServerlessExpress.createServer(app);
+
+//se descomenta esta linea para que el archivo api.test.js pueda testear los endpoints con el comando npm test
+// module.exports = server;
 
 exports.handler = (event, context) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
--- swappi/swappi.js
+++ swappi/swappi.js
@@ -2,19 +2,69 @@ const fetch = require('node-fetch');
 
 async function getPeople(id) {
     try {
-        let petition;
-        if (!id){
-            petition = 'https://swapi.py4e.com/api/people'
-        }else{
-            petition = `https://swapi.py4e.com/api/people/${id}`
+        var personas = {};
+        let people;
+        if (!id) {
+            people = 'https://swapi.py4e.com/api/people'
+        } else {
+            people = `https://swapi.py4e.com/api/people/${id}`
         }
-            
-
-        const response = await fetch(petition, {
+        const response = await fetch(people, {
             method: 'get'
         })
+
         const person = await response.json();
-        return person;
+        
+        personas["nombre"]= person.name
+        personas["talla"]= person.height
+        personas["peso"]= person.mass
+        personas["color_pelo"]= person.hair_color
+        personas["color_piel"]= person.skin_color
+        personas["color_ojo"]= person.eye_color
+        personas["cumpleano"]= person.birth_year
+        personas["genero"]= person.gender
+        personas["mundo_hogar"]= person.homeworld
+        personas["pelicula"]= person.films
+        personas["especie"]= person.species
+        personas["vehiculo"] =person.vehicles
+        personas["nave_estelar"] =person.starships
+        
+        return personas;
+    } catch (error) {
+        console.log(error);
+    }
+
+};
+async function getPlanet(id) {
+    try {
+        var planetas = {};
+        let planet;
+        if (!id) {
+            planet = 'https://swapi.py4e.com/api/planets/'
+        } else {
+            planet = `https://swapi.py4e.com/api/planets/${id}`
+        }
+        const response = await fetch(planet, {
+            method: 'get'
+        })
+
+        const planets = await response.json();
+        
+        planetas["clima"]= planets.climate
+        planetas["diametro"]= planets.diameter
+        planetas["gravedad"]= planets.gravity
+        planetas["nombre"]= planets.name
+        planetas["periodo_orbital"]= planets.orbital_period
+        planetas["poblacion"]= planets.population
+        planetas["residente"]= planets.residents
+        planetas["periodo_rotacion"]= planets.rotation_period
+        planetas["superficie_acuatica"]= planets.surface_water
+        planetas["terreno"]= planets.terrain
+ 
+        // for(data in person){
+        //     console.log(person[data])
+        //     }
+        return planetas;
     } catch (error) {
         console.log(error);
     }
@@ -22,5 +72,6 @@ async function getPeople(id) {
 };
 
 module.exports = {
-    getPeople
+    getPeople,
+    getPlanet
 }
